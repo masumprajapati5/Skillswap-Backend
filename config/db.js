2100374c66d40c6568bca0c8142061cb8cdb6176
masumprajapati5 <pm04092005@gmail.com>
2026-05-05 20:33:10 +0530

    deploy on render

--- config/db.js
+++ config/db.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
+
+// Fix for querySrv ECONNREFUSED on some networks
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/skillswap');
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/skillswap', {
+      family: 4
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  passwordHash: { type: String, required: true },
+  passwordHash: { type: String },
   avatar: { type: String },
   bio: { type: String },
   location: {
@@ -27,6 +27,33 @@ const userSchema = new mongoose.Schema({
   resetTokenExpiry: Date
 }, {
   timestamps: true
+});
+
+// Hash password before saving
+userSchema.pre('save', async function() {
+  if (!this.isModified('password') && !this.isModified('passwordHash')) {
+    return;
+  }
+
+  // If password is provided, hash it into passwordHash
+  if (this.password) {
+    const bcrypt = require('bcryptjs');
+    const salt = await bcrypt.genSalt(10);
+    this.passwordHash = await bcrypt.hash(this.password, salt);
+  }
+});
+
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  const bcrypt = require('bcryptjs');
+  return await bcrypt.compare(enteredPassword, this.passwordHash);
+};
+
+// Virtual for password to handle it during creation/updates
+userSchema.virtual('password').set(function(password) {
+  this._password = password;
+}).get(function() {
+  return this._password;
 });
 
 module.exports = mongoose.model('User', userSchema);
